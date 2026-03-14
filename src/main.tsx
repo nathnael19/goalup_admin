a4c907fbe503d65fde0d8b60ce072e15e68fd394
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-14 21:59:00 +0300

    Wrap App component with ToastProvider to enable toast notifications throughout the application.

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary";
 import App from "./App.tsx";
+import { ToastProvider } from "./context/ToastContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </ErrorBoundary>
   </StrictMode>,
 );
